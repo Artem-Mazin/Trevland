7584f36371233f17d397e837f18cace887f69e2d
Artem Mazin <minima1s@yandex.ru>
2022-11-03 15:51:21 +0300

    add switcher

--- js/script.js
+++ js/script.js
@@ -1,34 +1,71 @@
 "use strict"
 
 const offerCards = document.querySelectorAll('.offers__card'),
-  rightBtn = document.querySelector('.switcher__arrow_direction_right'),
-  leftBtn = document.querySelector('.switcher__arrow_direction_left');
+      rightBtn = document.querySelector('.switcher__arrow_direction_right'),
+      leftBtn = document.querySelector('.switcher__arrow_direction_left'),
+      stripes = document.querySelectorAll('.switcher__stripe_position_not-active');
 
 function hideCards() {
   offerCards.forEach(function (card) {
     card.style.display = "none";
   })
 }
-function showCard(i = 0) {
+
+let i = 0;
+function showCard(i) {
   offerCards[i].style.display = "block";
   offerCards[i].style.margin = "auto";
 }
 hideCards();
-showCard();
+showCard(i);
 
-let i = 0;
+
 function nextCard() {
   hideCards();
   showCard(i);
-  i++;
 }
+
+function prevCard() {
+  hideCards();
+  showCard(i);
+}
+
+let a = 1
+function switchStripe (a) {
+  stripes.forEach(function () {
+    stripes[a].classList.toggle('switcher__stripe_position_active')
+  })
+}
+switchStripe(a);
 
 function rightClick() {
   rightBtn.addEventListener('click', () => {
-    nextCard();
+    i++;
+    a++;
     if (i == offerCards.length) {
       i = 0;
     }
+    if (a == stripes.length) {
+      a = 0;
+    }
+    nextCard();
+    switchStripe(a);
   })
 }
 rightClick();
+
+function leftClick() {
+  leftBtn.addEventListener('click', () => {
+    i--;
+    a--;
+    if (i < 0) {
+      i = offerCards.length - 1;
+    }
+    if (a < 0) {
+      a == stripes.length - 1;
+    }
+    prevCard();
+    switchStripe(a);
+  })
+}
+leftClick();
